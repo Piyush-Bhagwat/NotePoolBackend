22b307cf9563a75ba2ab83305f3c7f96213a0133
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-05-14 00:21:13 +0530

    removed cors

--- index.js
+++ index.js
@@ -10,13 +10,22 @@ config({ path: "./config.env" });
 
 const app = express();
 
-app.use(
-    cors({
-        origin: ["https://notespool.netlify.app/", "http://localhost:3000", "*"],
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type"],
-    })
-);
+// app.use(
+//     cors({
+//         origin: ["https://notespool.netlify.app/", "http://localhost:3000", "*"],
+//         methods: ["GET", "POST"],
+//         allowedHeaders: ["Content-Type"],
+//     })
+// );
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://notespool.netlify.app"); // update to match the domain you will make the request from
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 app.use("/public", cors(), express.static("public"));
 app.use(expressUpload());
