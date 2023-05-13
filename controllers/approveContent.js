7d4b913456ca15b65056292fccc12e6a751fb573
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-05-13 15:24:40 +0530

    added delete function on disapproval

--- controllers/approveContent.js
+++ controllers/approveContent.js
@@ -2,22 +2,27 @@ import { deleteDoc, doc } from "firebase/firestore";
 import {
     addToTestData,
     db,
+    deleteNote,
     readHold,
 } from "../Database/Firebase/firebaseConnect.js";
+import { deleteObject, ref } from "firebase/storage";
+import { storage } from "../Database/Firebase/firebaseConnect.js";
 
 const approveContent = async (req, res) => {
     const fileId = req.query.id;
     const toSave = req.query.tosave;
 
-    await readHold(fileId).then((data) => {
+    await readHold(fileId).then(async (data) => {
         if(toSave === 'true'){
-            addToTestData(data);
+            await addToTestData(data);
             console.log(fileId, "Approved");
         } else {
             console.log(fileId, "Not Approved");
         }
         
-        deleteDoc(doc(db, "hold", fileId));
+        const objRef = ref(storage, data.fileLocation);
+        await deleteObject(objRef);
+        await deleteDoc(doc(db, "hold", fileId));
         res.status(200).redirect("https://notespool.netlify.app/adminpi");
     });
 };
